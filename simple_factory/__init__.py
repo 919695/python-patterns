88023d63774ff2d0829ae285347742cc5c102c53
919695 <919695@qq.com>
2016-01-07 19:26:39 +0800

    change simple_factory
    factory单独出来，更符合top原则

--- simple_factory/__init__.py
+++ simple_factory/__init__.py
@@ -1,8 +1,8 @@
 __author__ = 'wangke'
 
-from simple_factory.operation import Operation
+from simple_factory.operation import OperationFactory
 
 if __name__ == '__main__':
-    operatcion = Operation.factory('/')
+    operatcion = OperationFactory.create('/')
     sum = operatcion.get_result(0, 0)
     print(sum)
--- simple_factory/operation.py
+++ simple_factory/operation.py
@@ -2,28 +2,20 @@ __author__ = 'wangke'
 
 '''
 简单工厂模式
-
 模式特点：工厂根据条件产生不同功能的类。
-
 程序实例：四则运算计算器，根据用户的输入产生相应的运算类，用这个运算类处理具体的运算。
+
+---------------------------------------------
+
+Operation 抽象基类
+get_result 方法没有实现,在子类实现
+factory 标记为类方法,该方法在类未实例化前就可以使用
 
 '''
 
 
 class Operation(object):
     def __init__(self):
-        pass
-
-    @classmethod
-    def factory(self, ch):
-        if ch == '+':
-            return OperationAdd()
-        elif ch == '-':
-            return OperationSub()
-        elif ch == '*':
-            return OperationMul()
-        elif ch == '/':
-            return OperationDiv()
         pass
 
     def get_result(self, op1, op2):
@@ -52,3 +44,17 @@ class OperationDiv(Operation):
         except ZeroDivisionError:
             return 0
         return sum
+
+
+class OperationFactory(object):
+    @staticmethod
+    def create(ch):
+        if ch == '+':
+            return OperationAdd()
+        elif ch == '-':
+            return OperationSub()
+        elif ch == '*':
+            return OperationMul()
+        elif ch == '/':
+            return OperationDiv()
+        pass
